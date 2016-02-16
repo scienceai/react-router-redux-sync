af5c364aec338d7eeee504d1dafca1221b12a38b
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2016-02-16 14:56:17 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # react-router-redux-sync
-Sync params and location from react-router to a redux store
+
+Sync params and location from react-router to a redux store.
+
+## Usage
+
+### Higher order component
+
+```
+import { SyncRouting } from 'react-router-redux-sync';
+import { Router, Route, browserHistory } from 'react-router'
+import { Provider } from 'react-redux';
+import App from './App';
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={SyncRouting(App)}>
+         ...
+      </Route>
+    </Router>
+  </Provider>,
+  document.body
+);
+
+```
+
+### Reducers
+
+
+```
+import { locationReducer, paramsReducer } from 'react-router-redux-sync';
+```
---
+++ src/index.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import isEqual from 'lodash/isEqual';
+
+export const UPDATE_LOCATION = 'UPDATE_LOCATION';
+export const UPDATE_PARAMS = 'UPDATE_PARAMS';
+
+export function locationReducer(state = {}, action) {
+  if (action.type === UPDATE_LOCATION) {
+    return Object.assign({}, state, action.payload)
+  }
+  return state;
+}
+
+export function paramsReducer(state = {}, action) {
+  if (action.type === UPDATE_PARAMS) {
+    return Object.assign({}, state, action.payload)
+  }
+  return state;
+}
+
+export function SyncRouting(ComposedComponent) {
+
+  class Provider extends Component {
+    componentDidMount () {
+      const { store, router } = this.context;
+
+      store.dispatch({
+        type: UPDATE_PARAMS,
+        payload: this.props.params
+      });
+
+      function createUniqueKey(location) {
+        return router.createPath(location);
+      }
+
+      this.unsubscribeHistory = router.listen(nextLocation => {
+        const { location } = store.getState();
+        if (!location.pathname || createUniqueKey(location) !== createUniqueKey(nextLocation)) {
+          store.dispatch({
+            type: UPDATE_LOCATION,
+            payload: nextLocation
+          });
+        }
+      });
+    }
+
+    componentWillReceiveProps(nextProps) {
+      const { store } = this.context;
+
+      if (!isEqual(this.props.params, nextProps.params)) {
+        store.dispatch({
+          type: UPDATE_PARAMS,
+          payload: nextProps.params
+        });
+      }
+    }
+
+    componentWillUnmount() {
+      this.unsubscribeHistory();
+    }
+
+    render() {
+      return <ComposedComponent {...this.props}/>;
+    }
+  }
+
+  Provider.contextTypes = {
+    store: React.PropTypes.object.isRequired,
+    router: React.PropTypes.object.isRequired
+  };
+
+  return Provider;
+}
